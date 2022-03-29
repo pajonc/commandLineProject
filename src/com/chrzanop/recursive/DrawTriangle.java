08d2cd2c62b95d5032b0935147684efc5db7ed9f
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2022-03-29 22:03:37 +0200

    recursive

---
+++ src/com/chrzanop/recursive/DrawTriangle.java
@@ -0,0 +1,27 @@
+package com.chrzanop.recursive;
+
+public class DrawTriangle {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            paint(drawTriangle(i));
+        }
+
+    }
+
+    public static int drawTriangle(int size) {
+        if (size == 1) {
+            return 1;
+        }
+        return size + 2;
+    }
+
+    public static void paint(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/com/chrzanop/recursive/Fibonacci.java
@@ -0,0 +1,27 @@
+package com.chrzanop.recursive;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(4));
+    }
+
+    public static int fibonacci(int n)  {
+        if(n == 0) {
+            return 0;
+        }
+        else if(n == 1) {
+            return 1;
+        }
+        else{
+            return  fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+
+    }
+}
