53e56eab07e6fb44420bdbb3342c810af5ae72fa
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2022-03-29 22:03:12 +0200

    operator checks

---
+++ src/com/chrzanop/operator/ShiftOperator.java
@@ -0,0 +1,10 @@
+package com.chrzanop.operator;
+
+public class ShiftOperator {
+
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(4>>1);
+        System.out.println(4<<2);
+    }
+}
