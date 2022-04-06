fde7ec9b83a37cda91f9eb112bd62438e430337f
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2022-04-06 12:52:41 +0200

    Maximum Depth Of Binary Tree - sample

--- src/com/chrzanop/tree/MaximumDepthOfBinaryTree.java
+++ src/com/chrzanop/tree/MaximumDepthOfBinaryTree.java
@@ -1,17 +1,54 @@
 package com.chrzanop.tree;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 public class MaximumDepthOfBinaryTree {
 
-    public int maxDepth(TreeNode root) {
-        if(root==null) {
+    public static void main(String[] args) {
+        MaximumDepthOfBinaryTree mdoft = new MaximumDepthOfBinaryTree();
+        TreeNode root = mdoft.setupTreeNode();
+        System.out.println(mdoft.maxDepth(root));
+        System.out.println("-------------");
+//        mdoft.traverseBFS(root);
+    }
+
+    private TreeNode setupTreeNode() {
+        TreeNode tn11 = new TreeNode(11);
+        TreeNode tn9 = new TreeNode(9, tn11, null);
+        TreeNode tn13 = new TreeNode(13);
+        TreeNode tn15 = new TreeNode(15);
+        TreeNode tn20 = new TreeNode(20, tn13, tn15);
+        TreeNode treeNode = new TreeNode(3, tn9, tn20);
+        return treeNode;
+    }
+
+    public int maxDepth(TreeNode node) {
+        if (node == null) {
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) +1;
+        int left = maxDepth(node.left);
+        int right = maxDepth(node.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public void traverseBFS(TreeNode node) {
+        if(node==null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+
+            System.out.println(poll.val);
+            if(poll.left != null) {
+                queue.add(poll.left);
+            }
+            if(poll.right != null) {
+                queue.add(poll.right);
+            }
+
+        }
     }
 
     public class TreeNode {
