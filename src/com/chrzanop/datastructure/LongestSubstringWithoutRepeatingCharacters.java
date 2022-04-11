3331be6f55b881da9c178a05738127047599583b
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2022-04-11 08:30:33 +0200

    longest substring without repeating characters

---
+++ src/com/chrzanop/datastructure/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,47 @@
+package com.chrzanop.datastructure;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        lengthOfLongestSubstring("abcabcbb");
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int size = 1;
+        Set<String> windows = new HashSet<>();
+
+        char[] chars = s.toCharArray();
+
+        int j = 0;
+        String window = "";
+        for (int i = 0; i < chars.length; i++) {
+            if (i == 0) {
+                window = String.valueOf(chars[i]);
+                windows.add(window);
+                continue;
+            }
+            while (window.contains(String.valueOf(chars[i]))) {
+                j++;
+                window = buildWindow(chars, j, i-1);
+            }
+            window = buildWindow(chars, j, i);
+            windows.add(window);
+        }
+
+
+        return size;
+
+    }
+
+    private static String buildWindow(char[] chars, int j, int i) {
+        String window = "";
+        for (int k = j; k <= i; k++) {
+            window += String.valueOf(chars[k]);
+        }
+        return window;
+    }
+
+}
