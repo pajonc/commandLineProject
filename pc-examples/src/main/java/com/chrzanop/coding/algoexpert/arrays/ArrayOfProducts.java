47acba572bf6bcb4d077db7d0c870582661d3204
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2022-11-20 19:59:41 +0100

    array of products - simplified

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/arrays/ArrayOfProducts.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/arrays/ArrayOfProducts.java
@@ -7,32 +7,27 @@ public class ArrayOfProducts {
 
     public static void main(String[] args) {
 
-        ArrayOfProducts aop = new ArrayOfProducts();
+        ArrayOfProducts aoProducts = new ArrayOfProducts();
 
-        int[] input = new int[] {5, 1, 4, 2};
-        int[] expected = new int[] {8, 40, 10, 20};
+        int[] input = new int[]{5, 1, 4, 2};
+        int[] expected = new int[]{8, 40, 10, 20};
         int[] input3 = new int[]{-5, 2, -4, 14, -6};
         int[] expected3 = new int[]{672, -1680, 840, -240, 560};
 
-        int[] result = aop.arrayOfProducts(input);
-
+        int[] result = aoProducts.arrayOfProducts(input);
         for (int i = 0; i < result.length; i++) {
             assertTrue(result[i] == expected[i]);
         }
 
-        int[] result2 = aop.arrayOfProductsSolution2(input);
-
+        int[] result2 = aoProducts.arrayOfProductsSolution2(input);
         for (int i = 0; i < result2.length; i++) {
             assertTrue(result2[i] == expected[i]);
         }
 
-        int[] result3 = aop.arrayOfProductsSolution2(input3);
-
+        int[] result3 = aoProducts.arrayOfProductsSolution2(input3);
         for (int i = 0; i < result3.length; i++) {
             assertTrue(result3[i] == expected3[i]);
         }
-
-
 
     }
 
@@ -42,8 +37,8 @@ public class ArrayOfProducts {
         int[] products = new int[array.length];
 
         int currentProduct = 1;
-        for(int i = 0; i<array.length; i++) {
-            for(int j = 0; j<array.length; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
                 if (i != j) {
                     currentProduct = currentProduct * array[j];
                 }
@@ -52,11 +47,10 @@ public class ArrayOfProducts {
             currentProduct = 1;
         }
 
-        // Write your code here.
         return products;
     }
 
-    // O(3n) --> O(n) time | O(n) space
+    // O(3n) --> O(n) time | O(3n) --> O(n) space
     public int[] arrayOfProductsSolution2(int[] array) {
 
         int[] left = new int[array.length];
@@ -64,21 +58,41 @@ public class ArrayOfProducts {
         int[] products = new int[array.length];
 
         int currentLeft = 1;
-        for (int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             left[i] = currentLeft;
             currentLeft *= array[i];
         }
 
         int currentRight = 1;
-        for (int i=array.length-1; i>=0; i--) {
+        for (int i = array.length - 1; i >= 0; i--) {
             right[i] = currentRight;
             currentRight *= array[i];
         }
 
-        for(int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             products[i] = left[i] * right[i];
         }
 
+        return products;
+    }
+
+
+    // solution 2 simplified , // O(2n) --> O(n) time | O(n) space
+    public int[] arrayOfProducts3(int[] array) {
+
+        int[] products = new int[array.length];
+
+        int currentLeft = 1;
+        for (int i = 0; i < array.length; i++) {
+            products[i] = currentLeft;
+            currentLeft *= array[i];
+        }
+
+        int currentRight = 1;
+        for (int i = array.length - 1; i >= 0; i--) {
+            products[i] *= currentRight;
+            currentRight *= array[i];
+        }
 
         return products;
     }
