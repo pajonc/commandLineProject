12c38c0421cd016bfee2efc37ca8a266707b75a2
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2023-01-31 16:10:45 +0100

    NumberOfBinaryTreeTopologies

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/recursion/NumberOfBinaryTreeTopologies.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/recursion/NumberOfBinaryTreeTopologies.java
@@ -2,6 +2,9 @@ package com.chrzanop.coding.algoexpert.recursion;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertTrue;
 
 public class NumberOfBinaryTreeTopologies {
@@ -9,14 +12,21 @@ public class NumberOfBinaryTreeTopologies {
 
     @Test
     public void TestCase1() {
-        assertTrue(numberOfBinaryTreeTopologies(3) == 5);
+        assertTrue(numberOfBinaryTreeTopologiesNaive(3) == 5);
     }
 
 
     public static int numberOfBinaryTreeTopologies(int n) {
+        Map<Integer, Integer> cache = new HashMap<>();
+        cache.put(0, 1);
+        return numberOfBinaryTreeTopologies(n, cache);
+    }
 
-        if (n == 0) {
-            return 1;
+
+    private static int numberOfBinaryTreeTopologies(int n, Map<Integer, Integer> cache) {
+
+        if (cache.containsKey(n)) {
+            return cache.get(n);
         }
         int numberOfTrees = 0;
         for (int lSize = 0; lSize < n; lSize++) {
@@ -25,8 +35,25 @@ public class NumberOfBinaryTreeTopologies {
             int numberofRightTreeSize = numberOfBinaryTreeTopologies(rSize);
             numberOfTrees += numberOfLeftTreeSize * numberofRightTreeSize;
         }
+        cache.put(n, numberOfTrees);
         return numberOfTrees;
     }
+
+
+    public static int numberOfBinaryTreeTopologiesNaive(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int numberOfTrees = 0;
+        for (int lSize = 0; lSize < n; lSize++) {
+            int rSize = n - 1 - lSize;
+            int numberOfLeftTreeSize = numberOfBinaryTreeTopologiesNaive(lSize);
+            int numberofRightTreeSize = numberOfBinaryTreeTopologiesNaive(rSize);
+            numberOfTrees += numberOfLeftTreeSize * numberofRightTreeSize;
+        }
+        return numberOfTrees;
+    }
+
 
 
 }
