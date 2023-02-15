dea41090d72b34922805b91effab7189cc5562e7
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2023-02-15 16:46:51 +0100

    FirstNonRepeatingCharacter - second approach (brute force)

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/string/FirstNonRepeatingCharacter.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/string/FirstNonRepeatingCharacter.java
@@ -1,11 +1,27 @@
 package com.chrzanop.coding.algoexpert.string;
 
+import org.junit.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertTrue;
+
 public class FirstNonRepeatingCharacter {
 
+    @Test
+    public void TestCase1() {
+        String input = "abcdcaf";
+        int expected = 1;
+        int actual = firstNonRepeatingCharacter(input);
+        assertTrue(expected == actual);
+        int actual2 = firstNonRepeatingCharacterBruteForce(input);
+        assertTrue(expected == actual2);
+    }
 
+
+    // O(n) time | O(1) - n size of length of input characters
+    // space because the input string only has lowercase character of max size 26
     public int firstNonRepeatingCharacter(String string) {
 
         Map<Character, Integer> characterCounts = new HashMap<>();
@@ -19,5 +35,24 @@ public class FirstNonRepeatingCharacter {
         }
         return -1;
     }
+
+
+    // O(n^2) time | O(1) - n size of length of input characters
+    public int firstNonRepeatingCharacterBruteForce(String string) {
+
+        for (int i = 0; i < string.length(); i++) {
+            boolean foundDuplicate = false;
+            for (int j = 0; j < string.length(); j++) {
+                if(string.charAt(i) == string.charAt(j) && i != j) {
+                    foundDuplicate = true;
+                }
+            }
+            if(!foundDuplicate) return i;
+        }
+        return -1;
+    }
+
+
+
 
 }
