708b45d938c373c029cb545a9820ab911ef98ad5
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2023-04-13 21:22:44 +0200

    SameBSTs - more optimal solution based on indexes

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/binarysearchtrees/SameBSTs.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/binarysearchtrees/SameBSTs.java
@@ -6,16 +6,69 @@ import java.util.List;
 public class SameBSTs {
 
 
+    // O(n^2) time | O(d) space - where n is the number of nodes in each array and d is the depth of the
+    // BST that they represent
+    public static boolean sameBstsAlgo2(List<Integer> arrayOne, List<Integer> arrayTwo) {
+        return sameBstsAlgo2(arrayOne, arrayTwo, 0, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+    }
+
+    public static boolean sameBstsAlgo2(List<Integer> arrayOne, List<Integer> arrayTwo, int rootIdxOne, int rootIdxTwo, int minVal, int maxVal) {
+        // situation when there is no nodes for current root element
+        if (rootIdxOne == -1 || rootIdxTwo == -1) return rootIdxOne == rootIdxTwo;
+
+        // if root nodes differs return false (not same BSTs)
+        if (arrayOne.get(rootIdxOne).intValue() != arrayTwo.get(rootIdxTwo).intValue()) {
+            return false;
+        }
+
+        int leftRootIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal);
+        int leftRootIdxTwo = getIdxOfFirstSmaller(arrayTwo, rootIdxTwo, minVal);
+        int rightRootIdxOne = getIdxOfFirstBiggerOrGreater(arrayOne, rootIdxOne, maxVal);
+        int rightRootIdxTwo = getIdxOfFirstBiggerOrGreater(arrayTwo, rootIdxTwo, maxVal);
+
+        int current = arrayOne.get(rootIdxOne);
+
+        boolean leftAreSame = sameBstsAlgo2(arrayOne, arrayTwo, leftRootIdxOne, leftRootIdxTwo, minVal, current);
+        boolean rightAreSame = sameBstsAlgo2(arrayOne, arrayTwo, rightRootIdxOne, rightRootIdxTwo, current, maxVal);
+
+        return leftAreSame && rightAreSame;
+
+    }
+
+    private static int getIdxOfFirstSmaller(List<Integer> array, int staringIdx, int minVal) {
+        int currentValue = array.get(staringIdx).intValue();
+        for (int i = staringIdx + 1; i < array.size(); i++) {
+            if (array.get(i).intValue() < currentValue && array.get(i).intValue() >= minVal)
+                return i;
+        }
+        return -1;
+    }
+
+    private static int getIdxOfFirstBiggerOrGreater(List<Integer> array, int staringIdx, int maxVal) {
+        int currentValue = array.get(staringIdx).intValue();
+        for (int i = staringIdx + 1; i < array.size(); i++) {
+            if (array.get(i).intValue() >= currentValue && array.get(i).intValue() < maxVal)
+                return i;
+        }
+        return -1;
+    }
+
+
     // O(n^2) time | O(n^2) space
     public static boolean sameBsts(List<Integer> arrayOne, List<Integer> arrayTwo) {
+        // different arrays means not same BSTs (in other words different nodes structure)
         if (arrayOne.size() != arrayTwo.size()) {
             return false;
         }
 
+        // if no child nodes then both arrays should have same situation to meet same BSTs assumption,
+        // otherwise not same BSTs
         if (arrayOne.size() == 0 && arrayTwo.size() == 0) {
             return true;
         }
 
+        // root nodes should be same to meet same BSTs check
         if (arrayOne.get(0).intValue() != arrayTwo.get(0).intValue()) {
             return false;
         }
@@ -60,8 +113,8 @@ public class SameBSTs {
 
     private static List<Integer> getSmaller(List<Integer> array) {
         List<Integer> smaller = new ArrayList<>();
-        for (int i = 1; i < array.size() ; i++) {
-            if(array.get(i).intValue()< array.get(0).intValue()) {
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i).intValue() < array.get(0).intValue()) {
                 smaller.add(array.get(i));
             }
         }
@@ -70,8 +123,8 @@ public class SameBSTs {
 
     private static List<Integer> getBiggerOrEqual(List<Integer> array) {
         List<Integer> biggerOrEqual = new ArrayList<>();
-        for (int i = 1; i < array.size() ; i++) {
-            if(array.get(i).intValue()>= array.get(0).intValue()) {
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i).intValue() >= array.get(0).intValue()) {
                 biggerOrEqual.add(array.get(i));
             }
         }
--- pc-examples/src/test/java/com/chrzanop/coding/algoexpert/binarysearchtrees/SameBSTsTest.java
+++ pc-examples/src/test/java/com/chrzanop/coding/algoexpert/binarysearchtrees/SameBSTsTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.chrzanop.coding.algoexpert.binarysearchtrees.SameBSTs.sameBsts;
+import static com.chrzanop.coding.algoexpert.binarysearchtrees.SameBSTs.sameBstsAlgo2;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SameBSTsTest {
@@ -17,6 +18,7 @@ public class SameBSTsTest {
         List<Integer> arrayOne = new ArrayList<Integer>(Arrays.asList(10, 15, 8, 12, 94, 81, 5, 2, 11));
         List<Integer> arrayTwo = new ArrayList<Integer>(Arrays.asList(10, 8, 5, 15, 2, 12, 11, 94, 81));
         assertTrue(sameBsts(arrayOne, arrayTwo) == true);
+        assertTrue(sameBstsAlgo2(arrayOne, arrayTwo) == true);
     }
 
 }
