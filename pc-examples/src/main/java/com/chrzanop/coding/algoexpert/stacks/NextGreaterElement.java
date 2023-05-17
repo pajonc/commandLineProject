a9dd169d9b694712a1067c56792423c7b60f8594
pawel.d.chrzanowski@gmail.com <Pawel1@345%>
2023-05-17 17:12:11 +0200

    NextGreaterElement - more solutions

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/stacks/NextGreaterElement.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/stacks/NextGreaterElement.java
@@ -8,7 +8,7 @@ public class NextGreaterElement {
 
 
     // O(n) time | O(n) space - where n is the length of the array
-    public int[] nextGreaterElement(int[] array) {
+    public int[] nextGreaterElement_Algo1(int[] array) {
         int[] result = new int[array.length];
         Arrays.fill(result, -1);
 
@@ -29,5 +29,49 @@ public class NextGreaterElement {
 
         return result;
     }
+
+    // O(n) time | O(n) space - where n is the length of the array
+    // reverse order algorithm
+    public int[] nextGreaterElement_Algo2(int[] array) {
+        int[] result = new int[array.length];
+        Arrays.fill(result, -1);
+
+        Deque<Integer> stack = new LinkedList<>();
+
+        for (int i = 2 * array.length- 1; i>=0; i--) {
+            int circularIdx = i % array.length;
+            while (stack.size()>0) {
+                if (stack.peek() <= array[circularIdx]) {
+                    stack.pop();
+                } else {
+                    result[circularIdx] = stack.peek();
+                    break;
+                }
+            }
+            stack.push(array[circularIdx]);
+        }
+
+        return result;
+    }
+
+    // O(n^2) time | O(1) space - where n is the length of the array
+    public int[] nextGreaterElementComplex(int[] array) {
+        int[] result = new int[array.length];
+        Arrays.fill(result, -1);
+        for (int i = 0; i < array.length; i++) {
+            int j=1;
+            int currentElement = array[i];
+            while(j<array.length) {
+                int k = (i + j) % array.length;
+                if(currentElement<array[k]) {
+                    result[i] = array[k];
+                    break;
+                }
+                j++;
+            }
+        }
+        return result;
+    }
+
 
 }
--- pc-examples/src/test/java/com/chrzanop/coding/algoexpert/stacks/NextGreaterElementTest.java
+++ pc-examples/src/test/java/com/chrzanop/coding/algoexpert/stacks/NextGreaterElementTest.java
@@ -10,8 +10,12 @@ public class NextGreaterElementTest {
     public void TestCase1() {
         int[] input = new int[] {2, 5, -3, -4, 6, 7, 2};
         int[] expected = new int[] {5, 6, 6, 6, 7, -1, 5};
-        int[] actual = new NextGreaterElement().nextGreaterElement(input);
-        assert (expected.equals(actual));
+        int[] actual = new NextGreaterElement().nextGreaterElement_Algo1(input);
+        int[] actual2= new NextGreaterElement().nextGreaterElement_Algo2(input);
+        int[] actual3 = new NextGreaterElement().nextGreaterElementComplex(input);
+        assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual2);
+        assertArrayEquals(expected, actual3);
     }
 
 }
