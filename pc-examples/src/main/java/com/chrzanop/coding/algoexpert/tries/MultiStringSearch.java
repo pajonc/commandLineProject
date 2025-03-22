75019df8aeeb6470785e5bd6caaa77a3d2926970
Pawel Chrzanowski <pawelch@backbase.com>
2025-03-22 17:52:22 +0100

    MultiStringSearch 2 more versions

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/tries/MultiStringSearch.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/tries/MultiStringSearch.java
@@ -7,7 +7,80 @@ import java.util.Map;
 
 public class MultiStringSearch {
 
+    // O(bns) time | O(n) space
+    public static List<Boolean> multiStringSearchComplexAlgo(String bigString, String[] smallStrings) {
+        List<Boolean> solution = new ArrayList<>();
+        for (String smallString : smallStrings) {
+            solution.add(isInBigString(bigString, smallString));
+        }
+        return solution;
+    }
+
+    private static boolean isInBigString(String bigString, String smallString) {
+        for (int i = 0; i < bigString.length(); i++) {
+            if (i + smallString.length() > bigString.length()) {
+                break;
+            }
+            if (isInBigString(bigString, smallString, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isInBigString(String bigString, String smallString, int startIdx) {
+        int leftBigIdx = startIdx;
+        int rightBigIdx = startIdx + smallString.length() - 1;
+        int leftSmallIdx = 0;
+        int rightSmallIdx = smallString.length() - 1;
+        while (leftBigIdx <= rightBigIdx) {
+            if (bigString.charAt(leftBigIdx) != smallString.charAt(leftSmallIdx)
+                    || bigString.charAt(rightBigIdx) != smallString.charAt(rightSmallIdx)) {
+                return false;
+            }
+            leftBigIdx++;
+            rightBigIdx--;
+            leftSmallIdx++;
+            rightSmallIdx--;
+        }
+        return true;
+    }
+
+    public static List<Boolean> multiStringSearchComplex(String bigString, String[] smallStrings) {
+        List<Boolean> result = new ArrayList<>();
+
+        for (String smallString : smallStrings) {
+            int j = 0;
+            for (int k = 0; k < bigString.length(); k++) {
+                for (int i = k; i < bigString.length(); i++) {
+                    if (j < smallString.length()) {
+                        if (bigString.charAt(i) == smallString.charAt(j)) {
+                            j++;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                if (j == smallString.length()) {
+                    result.add(true);
+                    break;
+                }
+                j=0;
+
+            }
+            if (j != smallString.length()) {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+
+
     // O(b^2 +ns) time | O(b^2 + n ) space
+    // where b is the length of the bigString
+    // n is the number of smallStrings
+    // and s the longest size of smallest string
     public static List<Boolean> multiStringSearch(String bigString, String[] smallStrings) {
 
         List<Boolean> result = new ArrayList<>();
@@ -19,14 +92,14 @@ public class MultiStringSearch {
             for (int i = 0; i < small.length(); i++) {
                 char character = small.charAt(i);
                 TrieNode trieNode = tempRoot.children.get(character);
-                if(trieNode == null) {
+                if (trieNode == null) {
                     result.add(false);
                     isValid = false;
                     break;
                 }
                 tempRoot = trieNode;
             }
-            if(isValid) {
+            if (isValid) {
                 result.add(true);
             }
         }
--- pc-examples/src/test/java/com/chrzanop/coding/algoexpert/tries/MultiStringSearchTest.java
+++ pc-examples/src/test/java/com/chrzanop/coding/algoexpert/tries/MultiStringSearchTest.java
@@ -14,8 +14,50 @@ class MultiStringSearchTest {
         List<Boolean> output = MultiStringSearch.multiStringSearch(
                 "this is a big string",
                 new String[] {"this", "yo", "is", "a", "bigger", "string", "kappa"}
+
         );
         assertTrue(compare(output, expected));
+
+        boolean[] expected2 = {true, true, false, true, true, false};
+        List<Boolean> output2 = MultiStringSearch.multiStringSearch(
+        "abcdefghijklmnopqrstuvwxyz",
+                new String[] {"abc", "mnopqr", "wyz", "no", "e", "tuuv"}
+                );
+        assertTrue(compare(output2, expected2));
+    }
+
+    @Test
+    public void TestCase1Complex() {
+        boolean[] expected = {true, false, true, true, false, true, false};
+        List<Boolean> output = MultiStringSearch.multiStringSearchComplex(
+                "this is a big string",
+                new String[] {"this", "yo", "is", "a", "bigger", "string", "kappa"}
+        );
+        assertTrue(compare(output, expected));
+
+        boolean[] expected2 = {false};
+        List<Boolean> output2 = MultiStringSearch.multiStringSearchComplex(
+                "abcdefghijklmnopqrstuvwxyz",
+                new String[] { "tuuv"}
+        );
+        assertTrue(compare(output2, expected2));
+    }
+
+    @Test
+    public void TestCase1Algo() {
+        boolean[] expected = {true, false, true, true, false, true, false};
+        List<Boolean> output = MultiStringSearch.multiStringSearchComplexAlgo(
+                "this is a big string",
+                new String[] {"this", "yo", "is", "a", "bigger", "string", "kappa"}
+        );
+        assertTrue(compare(output, expected));
+
+        boolean[] expected2 = {true, true, false, true, true, false};
+        List<Boolean> output2 = MultiStringSearch.multiStringSearchComplexAlgo(
+                "abcdefghijklmnopqrstuvwxyz",
+                new String[] {"abc", "mnopqr", "wyz", "no", "e", "tuuv"}
+        );
+        assertTrue(compare(output2, expected2));
     }
 
     public boolean compare(List<Boolean> arr1, boolean[] arr2) {
