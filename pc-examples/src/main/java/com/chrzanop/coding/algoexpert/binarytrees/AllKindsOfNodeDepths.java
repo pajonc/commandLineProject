07000809b94364fff6bcd5a5648781d6562deb93
Pawel Chrzanowski <pawelch@backbase.com>
2025-06-11 14:30:04 +0200

    AllKindsOfNodeDepths

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/binarytrees/AllKindsOfNodeDepths.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/binarytrees/AllKindsOfNodeDepths.java
@@ -15,6 +15,13 @@ public class AllKindsOfNodeDepths {
         return sums.stream().reduce(0, (subtotal, element) -> subtotal + element);
     }
 
+    // O(n) time | O(h) space
+    public static int allKindsOfNodeDepthsFaster(BinaryTree root) {
+        List<Integer> sums = new ArrayList<>();
+        Pair<Integer, Integer> depthVsSum = dfsFormula(root,sums);
+        return sums.stream().reduce(0, (subtotal, element) -> subtotal + element);
+    }
+
     static void dfsInOrder(BinaryTree tree, List<Integer> sums) {
         if (tree == null) {
             return;
@@ -35,13 +42,44 @@ public class AllKindsOfNodeDepths {
         return depthOnNode;
     }
 
+    // nD = nDL + #L + nDR + #R
+    // nDL --> node depth Left
+    // nDR --> node depth Right
+    // #L  --> sum of nodes Left
+    // #R  --> sum of nodes Right
+    static Pair<Integer, Integer> dfsFormula(BinaryTree tree, List<Integer> sums) {
+        if (tree == null) {
+            return new Pair<>(0, 0); // depth 0
+        }
+        Pair<Integer, Integer> dfsLeft = dfsFormula(tree.left, sums);
+        int nodeLeft = dfsLeft.sumOfNodes;
+        int nDL = dfsLeft.depth;
+
+        Pair<Integer, Integer> dfsRight = dfsFormula(tree.right, sums);
+        int nodeRight = dfsRight.sumOfNodes;
+        int nDR = dfsRight.depth ;
+
+        sums.add(nDL + nDR + nodeLeft + nodeRight);
+        return new Pair<>(nDL + nDR + nodeLeft + nodeRight , nodeLeft + nodeRight + 1);
+    }
+
+    static class Pair<U, V> {
+        public final U depth;
+        public final V sumOfNodes;
+
+        private Pair(U depth, V sum) {
+            this.depth = depth;
+            this.sumOfNodes = sum;
+        }
+    }
 
     static class BinaryTree {
         int value;
         BinaryTree left;
         BinaryTree right;
 
-        int depth; // added
+        int depth = 0; // added
+        int sumOfNodes = 0;
 
         public BinaryTree(int value) {
             this.value = value;
--- pc-examples/src/test/java/com/chrzanop/coding/algoexpert/binarytrees/AllKindsOfNodeDepthsTest.java
+++ pc-examples/src/test/java/com/chrzanop/coding/algoexpert/binarytrees/AllKindsOfNodeDepthsTest.java
@@ -21,4 +21,19 @@ class AllKindsOfNodeDepthsTest {
         assertEquals(26, actual);
     }
 
+    @Test
+    public void TestCase2() {
+        var root = new AllKindsOfNodeDepths.BinaryTree(1);
+        root.left = new AllKindsOfNodeDepths.BinaryTree(2);
+        root.left.left = new AllKindsOfNodeDepths.BinaryTree(4);
+        root.left.left.left = new AllKindsOfNodeDepths.BinaryTree(8);
+        root.left.left.right = new AllKindsOfNodeDepths.BinaryTree(9);
+        root.left.right = new AllKindsOfNodeDepths.BinaryTree(5);
+        root.right = new AllKindsOfNodeDepths.BinaryTree(3);
+        root.right.left = new AllKindsOfNodeDepths.BinaryTree(6);
+        root.right.right = new AllKindsOfNodeDepths.BinaryTree(7);
+        int actual = AllKindsOfNodeDepths.allKindsOfNodeDepthsFaster(root);
+        System.out.println(actual);
+    }
+
 }
