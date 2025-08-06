b57e28646ebf052f819383e0d497728cc3c43733
Pawel Chrzanowski <pawelch@backbase.com>
2025-08-06 09:06:38 +0200

    LongestSubarrayWithSum update with algo expert code

--- pc-examples/src/main/java/com/chrzanop/coding/algoexpert/arrays/LongestSubarrayWithSum.java
+++ pc-examples/src/main/java/com/chrzanop/coding/algoexpert/arrays/LongestSubarrayWithSum.java
@@ -3,30 +3,53 @@ package com.chrzanop.coding.algoexpert.arrays;
 public class LongestSubarrayWithSum {
 
     public int[] longestSubarrayWithSum(int[] array, int targetSum) {
-        int i = 0, j=0;
-        int targetIdStart = i, targetIdEnd = j;
-        int windowLength = 0;
+        int[] indicies = new int[]{};
 
-        int targetSumCalc = 0;
-        for (int k = 0; k < array.length; k++) {
-            int value = array[k];
-            targetSumCalc += value;
-            if(targetSumCalc == targetSum) {
-                if(j-i > windowLength) {
-                    windowLength = j - i;
-                    targetIdStart = i;
-                    targetIdEnd = j;
-                }
-                targetSumCalc = targetSumCalc - array[i];
-                i = i+1;
-            } else if (targetSumCalc > targetSum) {
-                targetSumCalc = targetSumCalc - array[i];
-                i = i+1;
+        int currentSubarraySum = 0;
+        int startingIndex = 0;
+        int endingIndex = 0;
+
+        while (endingIndex < array.length) {
+            currentSubarraySum += array[endingIndex];
+            while (startingIndex < endingIndex && currentSubarraySum > targetSum) {
+                currentSubarraySum -= array[startingIndex];
+                startingIndex++;
             }
-            j = j + 1;
+
+            if (currentSubarraySum == targetSum) {
+                if (indicies.length == 0 || endingIndex - startingIndex > indicies[1] - indicies[0]) {
+                    indicies = new int[]{startingIndex, endingIndex};
+                }
+            }
+
+            endingIndex++;
         }
 
-        return new int[] {targetIdStart, targetIdEnd};
+
+//        int i = 0, j=0;
+//        int targetIdStart = i, targetIdEnd = j;
+//        int windowLength = 0;
+//
+//        int targetSumCalc = 0;
+//        for (int k = 0; k < array.length; k++) {
+//            int value = array[k];
+//            targetSumCalc += value;
+//            if(targetSumCalc == targetSum) {
+//                if(j-i > windowLength) {
+//                    windowLength = j - i;
+//                    targetIdStart = i;
+//                    targetIdEnd = j;
+//                }
+//                targetSumCalc = targetSumCalc - array[i];
+//                i = i+1;
+//            } else if (targetSumCalc > targetSum) {
+//                targetSumCalc = targetSumCalc - array[i];
+//                i = i+1;
+//            }
+//            j = j + 1;
+//        }
+
+        return indicies;
     }
 
 }
